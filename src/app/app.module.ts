56b9e35779f39e780f8009a795b5dea7658a5f60
amalmohann <amal.mohan@storilabs.com>
2021-10-23 11:40:42 +0530

    created the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AdminPanelComponent } from './pages/admin-panel/admin-panel.component';
+import { VoteComponent } from './pages/vote/vote.component';
+import { PostsComponent } from './pages/adminPanel/posts/posts.component';
+import { CategoryComponent } from './pages/adminPanel/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    HomeComponent,
+    AdminPanelComponent,
+    VoteComponent,
+    PostsComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
